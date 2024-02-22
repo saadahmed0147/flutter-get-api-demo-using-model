03b54296b94c9f2de0dae81c424a64b554c64d81
SaadAhmed <99617062+SaadAhmed012@users.noreply.github.com>
2024-02-23 04:18:54 +0500

    added post api

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     var data = jsonDecode(response.body.toString());
     if (response.statusCode == 200) {
+      postList.clear();
       for (Map i in data) {
         postList.add(PostModel.fromJson(i));
       }
@@ -37,6 +38,60 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.blue,
         title: const Text('POST API DEMO'),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPostApi(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Text('Loading');
+                  } else {
+                    return ListView.builder(
+                        itemCount: postList.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('CARD:${postList[index].id}',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20)),
+                                  const Text(
+                                    'TITLE:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Text(
+                                    postList[index].title.toString(),
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 18),
+                                  ),
+                                  const Text(
+                                    'DESCRIPTION:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Text(
+                                    postList[index].body.toString(),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  }
+                }),
+          )
+        ],
       ),
     );
   }
