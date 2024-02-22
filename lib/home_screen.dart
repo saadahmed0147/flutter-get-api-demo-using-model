3d9de273e7b173269ec3c6ca5baf38403369f573
SaadAhmed <99617062+SaadAhmed012@users.noreply.github.com>
2024-02-23 04:16:48 +0500

    added post api

---
+++ lib/home_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:post_api_demo/Models/post_model.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postList = [];
+
+  Future<List<PostModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('POST API DEMO'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
